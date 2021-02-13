cbc7cccce52e4c61c6d146514087e2c634e059eb
Yunchao Yao <y8yao@ucsd.edu>
2021-02-13 12:54:05 -0800

    added scripts

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,68 @@
 // product-item.js
 
+
 class ProductItem extends HTMLElement {
   // TODO
+  static item_count = 0;
+  static cart_count = document.getElementById('cart-count');
+  static cart_table = [];
+  constructor(index, id, title, price, description, category, image, start_in_cart) {
+    super();
+    let in_cart = start_in_cart;
+    let styling = document.createElement('link');
+    styling.setAttribute('rel', 'stylesheet');
+    styling.setAttribute('href', '../styles/styles.css');
+
+    let root = this.attachShadow({ mode: 'open' });
+    let product = document.createElement('li');
+    product.setAttribute('class', 'product');
+    let product_img = document.createElement('img');
+    product_img.setAttribute('src', image);
+    product_img.setAttribute('alt', title);
+    let product_title = document.createElement('p');
+    product_title.setAttribute('class', 'title');
+    product_title.innerHTML = title;
+    let product_price = document.createElement('p');
+    product_price.setAttribute('class', 'price');
+    product_price.innerHTML = price;
+    let add_button = document.createElement('button');
+    add_button.innerHTML = "Add to Cart";
+    if (in_cart === true) {
+      add_button.innerHTML = "Remove from cart";
+    }
+
+
+    product.appendChild(product_img);
+    product.appendChild(product_title);
+    product.appendChild(product_price);
+    product.appendChild(add_button);
+    root.appendChild(product);
+    root.appendChild(styling);
+
+    function add_item() {
+      if (in_cart === false) {
+        add_button.innerHTML = "Remove from cart";
+        in_cart = true;
+        ProductItem.item_count += 1;
+        ProductItem.cart_count.innerHTML = ProductItem.item_count;
+        ProductItem.cart_table[index] = true;
+      }
+      else if (in_cart === true) {
+        add_button.innerHTML = "Add to Cart";
+        in_cart = false;
+        ProductItem.item_count -= 1;
+        ProductItem.cart_count.innerHTML = ProductItem.item_count;
+        ProductItem.cart_table[index] = false;
+
+      }
+      localStorage.setItem('existance', ProductItem.cart_table.toString());
+      console.log(localStorage.getItem('existance'));
+      localStorage.setItem('cart_count', ProductItem.item_count.toString());
+    }
+    add_button.addEventListener('click', add_item);
+  }
+
+
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,62 @@
 // Script.js
+var container = document.getElementById('product-list');
+var item_list = [];
+
+function populate() {
+  for (let index in item_list) {
+    let product = item_list[index];
+    let start_in_cart = false;
+    if (ProductItem.cart_table[index] === true) {
+      start_in_cart = true;
+    }
+    let product_element = new ProductItem(
+      index,
+      product['id'],
+      product['title'],
+      product['price'],
+      product['description'],
+      product['category'],
+      product['image'],
+      start_in_cart
+    );
+    container.appendChild(product_element);
+
+  }
+  ProductItem.item_count = Number(localStorage.getItem('cart_count'));
+  ProductItem.cart_count.innerHTML = ProductItem.item_count;
+};
+
+function string_to_bool_array(stringified_array) {
+  let result = [];
+  let string_array = stringified_array.split(',');
+  for (let values in string_array) {
+    if (string_array[values] === "true") {
+      result.push(true);
+    }
+    else {
+      result.push(false);
+    }
+  }
+  return result;
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < data.length; i++) {
+        item_list.push(data[i]);
+        ProductItem.cart_table.push(false);
+        localStorage.setItem(i.toString(), JSON.stringify(data[i]));
+      }
+      if (localStorage.getItem('existance') != null) {
+        console.log(localStorage.getItem('existance'));
+        ProductItem.cart_table = string_to_bool_array(localStorage.getItem('existance'));
+      }
+      console.log(ProductItem.cart_table);
+      populate();
+    });
+
+
 });
